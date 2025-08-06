73e97325f6671b23645cc969adcad6f0a9013e15
siva_mac <sivakorea@gmail.com>
2025-08-06 14:54:56 +0100

    adding Static files

--- epos/settings.py
+++ epos/settings.py
@@ -113,9 +113,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
